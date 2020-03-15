0678479966fe302607913e2adbcec80bfb0d2c4f
Budsakorn <bbudsakornn@hotmail.com>
2020-03-15 12:03:06 +0700

    [create] typebox component

--- hcare/src/components/HelloWorld.vue
+++ hcare/src/components/HelloWorld.vue
@@ -31,6 +31,12 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+  
+    <button @click="onClick">
+        Submit
+    </button>
+
+
   </div>
 </template>
 
@@ -39,7 +45,12 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  methods: {
+    onClick() {
+     alert("Passss !!")
+    }
+  },
 }
 </script>
 
---
+++ hcare/src/views/ManageBooking.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <ServiceTypeBox :messageToComponents=messageToComponents />
+  </div>
+</template>
+
+<script>
+import ServiceTypeBox from "@/components/ServiceTypeBox.vue";
+
+export default {
+  data() {
+    return {
+      messageToComponents: [
+        { type_id: 1, type_name: "จิตแพทย์" },
+        { type_id: 2, type_name: "จิตวิทยา" }
+      ]
+    };
+  },
+  methods: {},
+  components: {
+    ServiceTypeBox
+  }
+};
+</script>
+
+<style>
+  
+
+
+</style>
