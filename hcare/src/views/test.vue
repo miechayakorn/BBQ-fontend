68a373da2599c96ae6ea3531b83f0e2e5af2ec1f
Mie Chayakorn <miechayakorn@gmail.com>
2020-04-24 00:34:05 +0700

    [Add] vue-datetime

--- hcare/src/views/test.vue
+++ hcare/src/views/test.vue
@@ -2,12 +2,49 @@
   <div>
     <span>{{ new Date() | moment("dddd, MMMM Do YYYY") }}</span>
     <div>
-      <input type="text" v-model="start" placeholder="start" />
-      <input type="text" v-model="end" placeholder="end" />
+      <input type="text" v-model="timeStart" />
+      <input type="text" v-model="timeEnd" />
       <input type="text" v-model="slot" placeholder="minute" />
       <button @click="getTimeStops">Gen</button>
     </div>
     {{ timeLoop }}
+    <div class="bg-primary p-5">
+      <p>Show Date</p>
+      <datetime
+        input-class="my-class"
+        class="theme-lavender"
+        placeholder="Select date"
+        format="dd-MM-yyyy"
+        v-model="date"
+      >
+      </datetime>
+    </div>
+    <div>
+      <p>Set minDate and maxDate</p>
+      <datetime
+        type="date"
+        v-model="minDate"
+        zone="Asia/Bangkok"
+        placeholder="start"
+      ></datetime>
+      <p>{{ minDate }}</p>
+      <datetime
+        type="date"
+        v-model="maxDate"
+        zone="Asia/Bangkok"
+        placeholder="end"
+      ></datetime>
+      <p>{{ maxDate }}</p>
+      <datetime
+        input-class="input"
+        :min-datetime="minDate"
+        :max-datetime="maxDate"
+        value-zone="local"
+        zone="local"
+      ></datetime>
+      <!-- var d = new Date();
+        var n = d.toISOString(); -->
+    </div>
   </div>
 </template>
 <script>
@@ -16,16 +53,18 @@ import moment from "moment";
 export default {
   data() {
     return {
-      start: "",
-      end: "",
+      timeStart: "",
+      timeEnd: "",
       slot: "",
+      minDate: "",
+      maxDate: "",
       timeLoop: []
     };
   },
   methods: {
     getTimeStops() {
-      let startTime = moment(this.start, "HH:mm:ss");
-      let endTime = moment(this.end, "HH:mm:ss");
+      let startTime = moment(this.timeStart, "HH:mm:ss");
+      let endTime = moment(this.timeEnd, "HH:mm:ss");
 
       if (endTime.isBefore(startTime)) {
         endTime.add(1, "day");
@@ -42,3 +81,16 @@ export default {
   }
 };
 </script>
+<style scope>
+.theme-orange .vdatetime-popup__header,
+.theme-orange .vdatetime-calendar__month__day--selected > span > span,
+.theme-orange .vdatetime-calendar__month__day--selected:hover > span > span {
+  background: #ff9800;
+}
+
+.theme-orange .vdatetime-year-picker__item--selected,
+.theme-orange .vdatetime-time-picker__item--selected,
+.theme-orange .vdatetime-popup__actions__button {
+  color: #ff9800;
+}
+</style>
