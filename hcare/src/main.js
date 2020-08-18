3f321015b334c1dc56ec96220e8b06c2cdb6cb95
Mie Chayakorn <miechayakorn@gmail.com>
2020-08-18 13:44:51 +0700

    [Edit] remove DATETIME dependency

--- hcare/src/main.js
+++ hcare/src/main.js
@@ -6,13 +6,10 @@ import store from './store'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import DataTable from 'v-data-table'
-import Datetime from 'vue-datetime'
-import 'vue-datetime/dist/vue-datetime.css'
 
 Vue.config.productionTip = false
 Vue.use(VueSweetalert2);
 Vue.use(DataTable)
-Vue.use(Datetime)
 Vue.use(require('vue-moment'));
 
 new Vue({
