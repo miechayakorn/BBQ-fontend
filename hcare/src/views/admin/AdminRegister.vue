0a38a2a9ee684027bb1895d5ed77a7a0f1c725d9
Mie Chayakorn <miechayakorn@gmail.com>
2020-09-09 14:21:14 +0700

    [Add] logic addRole

--- hcare/src/views/admin/AdminRegister.vue
+++ hcare/src/views/admin/AdminRegister.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="container mb-3 bg" v-if="!loading">
       <div class="row">
-        <div class="col-12 col-md-6">
+        <div class="col-12 col-md-6 d-none d-md-block">
           <div class="row">
             <div class="col-12">
               <div class="text-left text-title">
@@ -28,14 +28,18 @@
                     <td
                       style="text-align: left"
                     >{{dataPrepareSend.first_name}} {{dataPrepareSend.last_name}}</td>
-
                     <tr>
-                      <td>ตำแหน่ง</td>
+                      <td>เบอร์ติดต่อ</td>
                       <td style="text-align: left">{{dataPrepareSend.telephone}}</td>
                     </tr>
                     <tr>
-                      <td>ติดต่อ</td>
-                      <td style="text-align: left">{{dataPrepareSend.telephone}}</td>
+                      <td>ตำแหน่ง</td>
+                      <td style="text-align: left">
+                        <span
+                          v-for="(role, index) in dataPrepareSend.hc_role"
+                          :key="index"
+                        >{{role.role_name}}&#32;</span>
+                      </td>
                     </tr>
                   </table>
                 </div>
@@ -50,7 +54,7 @@
               <div class="form-group" style="margin-top:48px;">
                 <div class="text-left mb-2">
                   <div class="row">
-                    <div class="col-6">
+                    <div class="col-12 col-md-6">
                       <label for="firstName">ชื่อจริง</label>
                       <input
                         v-model="dataPrepareSend.first_name"
@@ -60,7 +64,7 @@
                         placeholder="ชื่อจริง"
                       />
                     </div>
-                    <div class="col-6">
+                    <div class="col-12 col-md-6">
                       <label for="lastName">นามสกุล</label>
                       <input
                         type="text"
@@ -81,7 +85,7 @@
                       />
                     </div>
 
-                    <div class="col-6 mt-3">
+                    <div class="col-12 col-md-6 mt-3">
                       <label for="inputTel">เบอร์โทรศัพท์</label>
                       <input
                         type="tel"
@@ -91,15 +95,25 @@
                         v-model="dataPrepareSend.telephone"
                       />
                     </div>
-                    <div class="col-6 mt-3">
-                      <label for="inputHNNumber">รหัสบุคคลากรมหาวิทยาลัย</label>
-                      <input type="number" class="form-control" id="inputHNNumber" />
+                    <div class="col-12 col-md-6 mt-3">
+                      <label for="inputHNNumber">เลขบัตรประชาชน / เลขบุคคลากร</label>
+                      <input
+                        v-model="dataPrepareSend.hn_number"
+                        type="number"
+                        class="form-control"
+                        id="inputHNNumber"
+                      />
                     </div>
-                    <div class="col-6 mt-3">
+                    <div class="col-12 col-md-6 mt-3">
                       <label for="inputPassword">รหัสผ่าน</label>
-                      <input type="password" class="form-control" id="inputPassword" />
+                      <input
+                        v-model="dataPrepareSend.password"
+                        type="password"
+                        class="form-control"
+                        id="inputPassword"
+                      />
                     </div>
-                    <div class="col-6 mt-3">
+                    <div class="col-12 col-md-6 mt-3">
                       <label for="inputConfirmPassword">ยืนยันรหัสผ่าน</label>
                       <input type="password" class="form-control" id="inputConfirmPassword" />
                     </div>
@@ -111,7 +125,7 @@
                           class="custom-select"
                           id="inputGroupSelect04"
                         >
-                          <option value disabled selected>-- กรุณาเลือกตำแหน่ง --</option>
+                          <option value selected disabled>-- กรุณาเลือกตำแหน่ง --</option>
                           <option
                             v-for="(role, index) in dataFetch.hc_role"
                             :key="index"
@@ -127,15 +141,27 @@
                         </div>
                       </div>
                     </div>
+                    <div class="col-12 mt-3" v-if="alertHcRole()">
+                      <div
+                        role="alert"
+                        class="alert alert-warning text-center"
+                      >กรุณาเลือกตำแหน่งให้เรียบร้อย</div>
+                    </div>
                     <div class="col-12 mt-3">
-                      <button class="btn div-showTag text-white text-left m-1">
+                      <button
+                        v-for="(selectRole, index) in dataPrepareSend.hc_role"
+                        :key="index"
+                        @click="removeRole(selectRole)"
+                        class="btn div-showTag text-white text-left m-1"
+                      >
                         <span class="mt-4 mb-4">
-                          หมอ
+                          {{selectRole.role_name}}
                           <i class="fas fa-times-circle ml-3"></i>
                         </span>
                       </button>
                     </div>
                   </div>
+                  {{dataPrepareSend}}
                 </div>
                 <button
                   @click="sendToBackend"
@@ -161,7 +187,7 @@ export default {
   data() {
     return {
       loading: false,
-      selectDropDown: { rn_id: "", role_name: "" },
+      selectDropDown: null,
       dataPrepareSend: {
         account_id: "",
         first_name: "",
@@ -170,7 +196,7 @@ export default {
         email: "",
         telephone: "",
         password: "",
-        hc_role: [], //id
+        hc_role: [],
       },
       dataFetch: {
         hc_role: [],
@@ -201,9 +227,66 @@ export default {
   },
 
   methods: {
-    sendToBackend() {},
+    sendToBackend() {
+      if (
+        this.dataPrepareSend.account_id != "" &&
+        this.dataPrepareSend.first_name != "" &&
+        this.dataPrepareSend.last_name != "" &&
+        this.dataPrepareSend.hn_number != "" &&
+        this.dataPrepareSend.email != "" &&
+        this.dataPrepareSend.telephone != "" &&
+        this.dataPrepareSend.password != "" &&
+        this.dataPrepareSend.hc_role.length != 0
+      ) {
+        let arrayRoleId = [];
+        this.dataPrepareSend.hc_role.forEach((role) => {
+          arrayRoleId.push(role.rn_id);
+        });
+        alert(arrayRoleId);
+
+
+
+
+
+
+
+        
+      } else {
+        this.$swal({
+          icon: "warning",
+          title: "คำเตือน",
+          text: "กรุณากรอกข้อมูลให้ครบ",
+        });
+      }
+    },
     addRole() {
-      console.log(this.selectDropDown.role_name);
+      if (this.selectDropDown != null) {
+        this.dataPrepareSend.hc_role.push(this.selectDropDown);
+        this.dataFetch.hc_role = Object.values(
+          Object.fromEntries(
+            Object.entries(this.dataFetch.hc_role).filter(
+              ([key, val]) => val.rn_id !== this.selectDropDown.rn_id
+            )
+          )
+        );
+        this.selectDropDown = null;
+      }
+    },
+    removeRole(selectRole) {
+      this.dataPrepareSend.hc_role = Object.values(
+        Object.fromEntries(
+          Object.entries(this.dataPrepareSend.hc_role).filter(
+            ([key, val]) => val.rn_id !== selectRole.rn_id
+          )
+        )
+      );
+      this.dataFetch.hc_role.push(selectRole);
+    },
+    alertHcRole() {
+      if (this.dataPrepareSend.hc_role.length != 0) {
+        return false;
+      }
+      return true;
     },
   },
   components: {
