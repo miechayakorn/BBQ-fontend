a2283a38b56ff7be9aeef30d21a97b17392fd911
Mie Chayakorn <miechayakorn@gmail.com>
2020-10-07 19:26:04 +0700

    [Edit] initialial oauth

--- hcare/src/views/Login.vue
+++ hcare/src/views/Login.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <logoHeader />
-    <div class="container fixed-container mb-3 bg" style="margin-top:40px">
+    <div class="container fixed-container mb-3 bg" style="margin-top: 40px">
       <SendMailForm v-if="!email" v-on:email="getEmail" />
       <ComfirmOTPForm v-else-if="email" :email="email" />
+      <button @click="adal" class="btn btn-primary">OAuth</button>
+      <button @click="getProfile" class="btn btn-primary">getProfile</button>
+      <button @click="adalLogout" class="btn btn-primary">Logout</button>
     </div>
-      <footer class="footer">
-        <div class="container">
-          Don’t have an account? <router-link to="/register">Register</router-link>
-        </div>
-      </footer>
+    <footer class="footer">
+      <div class="container">
+        Don’t have an account?
+        <router-link to="/register">Register</router-link>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -18,23 +22,36 @@ import logoHeader from "@/components/svg/logoHeader.vue";
 import SendMailForm from "@/components/login/SendMailForm.vue";
 import ComfirmOTPForm from "@/components/login/ComfirmOTPForm";
 import { waiting, errorSWAL, successSWAL } from "@/utility/swal.js";
+import authentication from "@/utility/authentication";
 
 export default {
   data() {
     return {
-      email: null
+      email: null,
     };
   },
   methods: {
+    adal() {
+      authentication.initialize();
+      authentication.getUserProfile();
+    },
+    getProfile() {
+      authentication.initialize();
+      console.log(authentication.getUserProfile().name);
+    },
+    adalLogout() {
+      authentication.initialize();
+      authentication.signOut();
+    },
     getEmail(email) {
       this.email = email;
-    }
+    },
   },
   components: {
     logoHeader,
     SendMailForm,
-    ComfirmOTPForm
-  }
+    ComfirmOTPForm,
+  },
 };
 </script>
 
@@ -44,7 +61,7 @@ export default {
   position: fixed;
   bottom: 0;
   line-height: 60px;
-  background-color: #F9F9FC;
+  background-color: #f9f9fc;
 }
 /* enable absolute positioning */
 .inner-addon {
