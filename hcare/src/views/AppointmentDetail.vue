ecf125853520fedc74be783b70de32e2a7691ff9
Budsakorn <bbudsakornn@hotmail.com>
2020-11-18 17:45:34 +0700

    [edit] edit border button

--- hcare/src/views/AppointmentDetail.vue
+++ hcare/src/views/AppointmentDetail.vue
@@ -62,27 +62,28 @@
                   </button>
                 </a>
               </div>
+            </div>
+          </div>
+          <div class="row" style="text-align: center">
+            <div class="col-12" @click="$router.go(-1)">
               <div
                 v-show="dataFetch.link_meeting == null && dateCanCancel"
-                class="col-12"
+               
               >
                 <button
                   class="btn btnBlock btnCancel fixed-button mb-2"
                   @click="cancelAppointment"
+                  style="border-radius: 10px"
                 >
-                  <span style="font-weight: 900; color: white">ยกเลิกนัด</span>
+                  <span style="font-weight: 900; color: white; ">{{ $t("cancel") }}</span>
                 </button>
               </div>
-            </div>
-            <div class="row" style="text-align: center">
-              <div class="col-12" @click="$router.go(-1)">
-                <button
-                  class="btn btnBlock btn-primary btn-back fixed-button mb-2"
-                  style="border-radius: 31px"
-                >
-                  <span style="font-weight: 900">{{ $t("back") }}</span>
-                </button>
-              </div>
+              <button
+                class="btn btnBlock btn-primary btn-back fixed-button mb-2"
+                style="border-radius: 10px"
+              >
+                <span style="font-weight: 900">{{ $t("back") }}</span>
+              </button>
             </div>
           </div>
         </div>
@@ -114,20 +115,20 @@ export default {
             type_name: "",
             date: "",
             time_in: "",
-            dateformat: "",
-          },
+            dateformat: ""
+          }
         ],
         link_meeting: "",
         doctor_id: null,
         doctor_firstname: "",
-        doctor_lastname: "",
-      },
+        doctor_lastname: ""
+      }
     };
   },
   components: {
     AppointmentCard,
     VclFacebook,
-    VclList,
+    VclList
   },
   async mounted() {
     this.loading = true;
@@ -135,10 +136,10 @@ export default {
       .get(
         `${process.env.VUE_APP_BACKEND_URL}/appointment/detail/${this.$route.params.id}`,
         {
-          headers: { Authorization: `Bearer ${this.$store.state.token}` },
+          headers: { Authorization: `Bearer ${this.$store.state.token}` }
         }
       )
-      .then((res) => {
+      .then(res => {
         if (res.data.account_id) {
           this.dataFetch.appointmentCard[0].account_id = res.data.account_id;
           this.dataFetch.appointmentCard[0].hn_number = res.data.hn_number;
@@ -159,10 +160,10 @@ export default {
           this.$router.push("/");
         }
       })
-      .catch((error) => {
+      .catch(error => {
         console.log("===== Backend-error ======");
         console.error(error.response);
-        this.$router.go(-1)
+        this.$router.go(-1);
       });
     this.loading = false;
   },
@@ -182,7 +183,7 @@ export default {
         this.dateCanCancel = d.toLocaleDateString("th-TH", {
           year: "numeric",
           month: "long",
-          day: "numeric",
+          day: "numeric"
         });
       }
       return result;
@@ -195,8 +196,8 @@ export default {
         showCloseButton: true,
         confirmButtonColor: "#FF4F5B",
         confirmButtonText: "ยืนยัน",
-        footer: "ระบบจะไม่สามารถคืนการนัดได้",
-      }).then((result) => {
+        footer: "ระบบจะไม่สามารถคืนการนัดได้"
+      }).then(result => {
         if (result.value) {
           this.$swal({
             title: "กรุณารอสักครู่",
@@ -204,38 +205,38 @@ export default {
             allowOutsideClick: false,
             onOpen: () => {
               this.$swal.showLoading();
-            },
+            }
           });
           axios
             .post(
               `${process.env.VUE_APP_BACKEND_URL}/appointment/cancel`,
               {
-                booking_id: this.dataFetch.appointmentCard[0].booking_id,
+                booking_id: this.dataFetch.appointmentCard[0].booking_id
               },
               {
-                headers: { Authorization: `Bearer ${this.$store.state.token}` },
+                headers: { Authorization: `Bearer ${this.$store.state.token}` }
               }
             )
-            .then((res) => {
+            .then(res => {
               this.$swal({
                 toast: true,
                 position: "top-end",
                 showConfirmButton: false,
                 timer: 3000,
                 icon: "success",
-                title: "ยกเลิกการนัดสำเร็จ",
+                title: "ยกเลิกการนัดสำเร็จ"
               });
               this.$router.push("/appointment");
             })
-            .catch((error) => {
+            .catch(error => {
               console.log("===== Backend-error ======");
               console.error(error.response);
               this.$swal({ ...errorSWAL });
             });
         }
       });
-    },
-  },
+    }
+  }
 };
 </script>
 
